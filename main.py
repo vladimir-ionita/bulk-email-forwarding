62db45b924ddb40f9b03a7226210173018707d84
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-22 12:54:23 -0500

    Fetch the list of emails

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ import imaplib
 imap_host = "imap.gmail.com"
 username = "email@gmail.com"
 password = "password"
+search_criteria = "ALL"
 
 # Connect through IMAP
 imap_client = imaplib.IMAP4_SSL(imap_host)
 imap_client.login(username, password)
+
+# Fetch messages' ID list
+status, _ = imap_client.select("INBOX", readonly=True)
+if status != "OK":
+    raise Exception("Could not select connect to INBOX.")
+
+status, data = imap_client.search(None, search_criteria)
+if status != "OK":
+    raise Exception("Could not search for emails.")
+
+messages_id_list = data[0].decode("utf-8").split(' ')
